dd22cedaaa4c476b03491a99bc27117b631c9b22
vikas rathee <vikasrathee3011@gmail.com>
2018-10-21 22:40:57 +0530

    Update gbus_count.py

--- practice_round/A_gbus_count/gbus_count.py
+++ practice_round/A_gbus_count/gbus_count.py
@@ -23,3 +23,17 @@ def Gbus_Count():
                         D[C[m]] += 1            # storing all the results in a dictonary D
         print("Case #{}: {}".format(i+1, ' '.join(str(x) for x in D.values())))
 Gbus_Count()
+
+
+#   few important things are:
+#       here the time complexcity is order of n^2, "O(n^2)" if you can find the technique to decrease its time than you are welcome.
+#       as I have written this code for beginners, but have you think a little beyond it, here I have created a dictonary do you really need this,
+#       think of it. the solution is:
+
+#       D = list(), result = 0                              instead of dict we created a list and variable name result = 0
+#       for m in range(P):
+#           for in range(N):
+#               if A[2*n]<=C[m]<=A[2*n+1]:
+#                   result += 1
+#               D.append(result)
+#       print("Case #{}: {}".format(i+1, *D)
