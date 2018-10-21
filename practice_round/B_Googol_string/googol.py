ca670064133c14d7e7d4b19a2e500f2d839d5827
vikas rathee <vikasrathee3011@gmail.com>
2018-10-21 23:50:12 +0530

    Update googol.py

--- practice_round/B_Googol_string/googol.py
+++ practice_round/B_Googol_string/googol.py
@@ -1,1 +1,13 @@
+def string(K):
+    n = 1
+    while n*2 <= K:
+        n *= 2
+    if n == K:
+        return 0
+    return(1 - string(n*2 - K))
 
+def Googol_string():
+    T = int(input())
+    for i in range(T):
+        print("Case #{}: {}".format(i+1, string(int(input()))))
+Googol_string()
